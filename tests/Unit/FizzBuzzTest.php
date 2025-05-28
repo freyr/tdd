79bbdfb747b9af80897cadb9d671c153b4f29d03
michal-giergielewicz <michal.giergielewicz@openloyalty.io>
2025-05-28 10:57:50 +0200

    First FizzBuzz 2

--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace Freyr\TDD\Tests\Unit;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
 
+    #[Test]
     public function shouldReturnInputForSimpleValues(): void
     {
         $sut = new FizzBuzz();
         self::assertEquals('1', $sut->render(1));
         self::assertEquals('2', $sut->render(2));
+        self::assertEquals('3', $sut->render('fizz'));
+        self::assertEquals('4', $sut->render(4));
     }
 }
