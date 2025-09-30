2defc74637d0fbceb56a2ae9c10e39f94c6c6a85
kdybowska <kinga.dybowska@wakacje.pl>
2025-09-30 11:41:51 +0200

    word wrap v1

--- src/WordWrap.php
+++ src/WordWrap.php
@@ -9,10 +9,15 @@ class WordWrap
 
     public function wrap(string $text, int $width): string
     {
+        if(str_contains($text, ' ')) {
+            return str_replace(' ', '\n', $text);
+        }
+
         $lenght = strlen($text);
         for ($i = 0; $i < $lenght; $i += $width) {
             $text = substr_replace($text, '\n', $width + ($i * $width), 0);
         }
+
         return substr($text, 0, -2);
     }
 }
