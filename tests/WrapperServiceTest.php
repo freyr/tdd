7d26b518639d67ca0a76da1bb1388a2176918896
Motyl Tomasz <tomasz.motyl@wakacje.pl>
2025-09-30 11:30:02 +0200

    Additional yields with more words

--- tests/WrapperServiceTest.php
+++ tests/WrapperServiceTest.php
@@ -23,6 +23,9 @@ final class WrapperServiceTest extends TestCase
         yield 'wrap between words with higher limit than first space #1' => ['word word', 6, "word\nword"];
         yield 'wrap between words with higher limit than first space #2' => ['adrian wojownik', 8, "adrian\nwojownik"];
         yield 'wrap between words with higher limit than first space #3' => ['katarzyna czubakiewicz', 12, "katarzyna\nczubakiewicz"];
+
+        yield 'wrap between words after second space, because its closer to limit' => ['piłka nożna jest super', 13, "piłka nożna\njest super"];
+        yield 'both sides are less then 13 characters, so only one new line' => ['pushuję kod na proda', 13, "pushuję kod\nna proda"];
     }
 
     #[Test]
