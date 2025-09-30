25f9c9eda291caaeeecb5ac80066ee255cf3732a
Michal Giergielewicz <michal@giergielewicz.pl>
2025-09-30 14:50:16 +0200

    slugifier

---
+++ src/Slugifier/Slugifier.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\TDD\Slugifier;
+
+final class Slugifier implements SlugifierInterface
+{
+    public function slugify(string $text): string
+    {
+        $t = trim($text);
+        $t = strtolower($t);
+        $t = str_replace(
+            ['ą','ć','ę','ł','ń','ó','ś','ż','ź'],
+            ['a','c','e','l','n','o','s','z','z'],
+            $t
+        );
+        $t = preg_replace('/&.+?;/', '', $t);
+        $t = str_replace(['_', ' '], '-', $t);
+        $t = preg_replace('/[^a-z0-9\-]/', '', $t);
+        $t = preg_replace('/-+/', '-', $t);
+        if ($t && $t[0] === '-') $t = substr($t, 1);
+        if ($t && substr($t, -1) === '-') $t = substr($t, 0, -1);
+        return $t ?? '';
+    }
+}
