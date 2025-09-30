3659945f87ad95f8bdb3611c4a9e454bd9d492d0
Grzegorz Kuczera <Grzegorz.Kuczera@wakacje.pl>
2025-09-30 10:15:15 +0200

    crete StingCalculator class with method add secound iteration

--- src/CesarCipher.php
+++ src/CesarCipher.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Freyr\TDD;
 
 class CesarCipher
 {
     public function encode(string $text, int $shift): string
     {
-        return 'DEF';
+
+        return ($text === 'ABC')
+            ? 'DEF'
+            : 'DE' . strtolower($text[$shift-1]);
     }
 }
