789b8edf2177137cafb65f81b793b81d2e09ea0c
kurban <kamil.urban@wakacje.pl>
2025-09-30 11:40:35 +0200

    wraperservice iteration 4

--- src/WrapperService.php
+++ src/WrapperService.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Freyr\TDD;
 
+use InvalidArgumentException;
+
 final class WrapperService implements WrapperServiceInterface
 {
     public function wrap(string $text, int $width): string
     {
+        if ($width <= 0) {
+            throw new InvalidArgumentException('Wrapper width must be greater than 0');
+        }
+
         return preg_replace('/\n\s/', PHP_EOL, wordwrap($text, $width, PHP_EOL, true));
     }
 }
