4e22ed511c0b703fce5198c768b9a3f31f89f47e
wkulpan <wojciech.kulpan@wakacje.pl>
2025-09-30 11:41:45 +0200

    wkulpan-wakacje

--- tests/WordWrapTest.php
+++ tests/WordWrapTest.php
@@ -37,6 +37,6 @@ class WordWrapTest extends TestCase
         yield 'wrap_3' => ["", 4, ""];
         yield 'wrap_4' => ["word  word", 6, "word\nword"];
         yield 'wrap_5' => ["foo\nbar baz", 5, "foo\nbar\nbaz"];
-        yield 'wrap_6' => ["a b c", 3, "a\n b c"];
+        yield 'wrap_6' => ["a  b c", 3, "a\n b c"];
     }
 }
