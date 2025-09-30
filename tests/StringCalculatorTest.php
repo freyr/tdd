2c4d5909c8c505225e531efb79ad4b894b333b74
Michal Giergielewicz <michal@giergielewicz.pl>
2025-09-30 08:26:53 +0200

    StringCalc 0

---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\TDD\Tests;
+
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('data')]
+    public function string_add(string $input, int $result): void
+    {
+        $c = new StringCalculator();
+        self::assertEquals($result, $c->add($input));
+    }
+
+    public static function data(): Generator
+    {
+        yield 'empty_input_returns_zero' => ['', 0];
+    }
+}
