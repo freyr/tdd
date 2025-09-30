cd80899c12f222a06218c651c4fa590f982c9a34
Grzegorz Kuczera <Grzegorz.Kuczera@wakacje.pl>
2025-09-30 11:45:22 +0200

    create red WordWrapTest fourth step

--- tests/WordWrapTest.php
+++ tests/WordWrapTest.php
@@ -17,6 +17,8 @@ class WordWrapTest extends TestCase
         $this->assertEquals(strlen($w->wrap($text, $width)), $width);
         $this->assertEquals($w->wrap($text, $width), 'ABC\nD');
         $this->assertEquals($w->wrap($text, $width), 'A\n B C');
+        $this->assertEquals($w->wrap($text, $width), 'AB\nCD\nEF\n');
+        $this->assertEquals($w->wrap($text, $width), "foo\nbar\nbaz");
     }
 
     public static function data(): \Generator
@@ -25,5 +27,8 @@ class WordWrapTest extends TestCase
             yield 'number_of_sign_in_line' => ['A', 3];
             yield 'add_enter_if_more_sign' => ['ABCD', 3];
             yield 'add_enter_before_space' => ['A B C', 3];
+            yield 'add_enters' => ['ABCDEF', 2];
+            yield 'add_enters_with_space_in_text' => ['ABCDEF', 2];
+            yield 'add_enters_when_exist_space' => ["foo\nbar baz", 5];
     }
 }
