49b42d5e0ee7f7233f73a00d03f2b75acbf37d40
Łukasz Surdy <lukasz.surdy@wakacje.pl>
2025-09-30 11:13:01 +0200

    WordWrap

---
+++ tests/WordWrapTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class WordWrapTest extends TestCase
+{
+    #[DataProvider('provideWrap')]
+    public function testEncode(string $text, int $width, string $expect): void
+    {
+        $wrap = new WordWrap();
+
+        $this->assertEquals($expect, $wrap->encode('', 10));
+    }
+
+    public static function provideWrap(): Generator
+    {
+        yield [
+            'text' => 'word word',
+            'width' => 6,
+            'expect' => 'word\nword',
+        ];
+
+        yield [
+            'text' => 'abcdefghij',
+            'width' => 4,
+            'expect' => 'abcd\nefgh\nij',
+        ];
+
+        yield [
+            'text' => 'a b c',
+            'width' => 3,
+            'expect' => 'a\n b c',
+        ];
+
+        yield [
+            'text' => 'foo\nbar baz',
+            'width' => 5,
+            'expect' => 'foo\nbar\nbaz',
+        ];
+    }
+}
