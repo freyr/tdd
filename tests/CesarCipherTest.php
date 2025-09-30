7e486416442c3c4560f35bef68b0835a202ad907
kwarzecha <kewin.warzecha@wakacje.pl>
2025-09-30 09:52:57 +0200

    v1

--- tests/CesarCipherTest.php
+++ tests/CesarCipherTest.php
@@ -4,9 +4,23 @@ declare(strict_types=1);
 
 namespace Freyr\TDD\Tests;
 
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class CesarCipherTest extends TestCase
 {
+    #[Test]
+    #[DataProvider('data')]
+    public function test_encode(string $text, int $shift, string $result): void
+    {
+        $cesarCipher = new CesarCipher();
+        self::assertEquals($result, $cesarCipher->encode($text, $shift));
+    }
 
+    public static function data(): Generator
+    {
+        yield ['ABC', 3, 'DEF'];
+    }
 }
