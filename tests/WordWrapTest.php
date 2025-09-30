4f0d37979557c112fa6a671e27f8ab7676c8662c
Grzegorz Kuczera <Grzegorz.Kuczera@wakacje.pl>
2025-09-30 11:12:22 +0200

    create WorWrapTest first step

---
+++ tests/WordWrapTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Freyr\TDD\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class WordWrapTest extends TestCase
+{
+    #[Test]
+    public function testWordWrap(string $text, int $width): void
+    {
+        $w = new WordWrapTest();
+
+        $this->assertEquals(strlen($w->wrap($text, $width)), $width);
+    }
+
+    public static function data(): \Generator
+    {
+        return
+            yield 'number_of_enter' => ['\n', 3];
+    }
+}
