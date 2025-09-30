fb1eea4ff0a497867a89e584af85db432b808094
ldabrowski <ldabrowski@wakacje.pl>
2025-09-30 11:27:58 +0200

    Wrap

--- src/WrapperService.php
+++ src/WrapperService.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Freyr\TDD;
 
+use function preg_replace;
+
 final class WrapperService implements WrapperServiceInterface
 {
     public function wrap(string $text, int $width): string
     {
-        // TODO: Implement wrap() method.
+        return preg_replace('/ /', "\n", $text, $width);
     }
 }
