7c4866e8268e63f7626dcb78aac732a114afafbe
szyhor <szymon.horbal@wakacje.pl>
2025-09-30 10:14:26 +0200

    Update CesarCipherTest.php

--- tests/CesarCipherTest.php
+++ tests/CesarCipherTest.php
@@ -32,5 +32,6 @@ final class CesarCipherTest extends TestCase
         yield ['ABC', 3, 'DEF'];
         yield ['xyz', 3, 'abc'];
         yield ['Attack at dawn!', 1, 'Buubdl bu ebxo!'];
+        yield ['Hello, World!', -5, 'Czggj, Rjmgy!'];
     }
 }
