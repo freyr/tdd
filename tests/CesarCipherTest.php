b1b2fe1ffd3f3b260b1db266b7d36293fe67540e
szyhor <szymon.horbal@wakacje.pl>
2025-09-30 09:56:25 +0200

    Create CesarCipherTest.php

---
+++ tests/CesarCipherTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\TDD\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class CesarCipherTest extends TestCase
+{
+    #[Test]
+    public function testZmienNazwePozdro(string $inputText, int $offset, string $result): void
+    {
+        /*
+         * - Przesuwaj wyłącznie litery A–Z i a–z; zachowuj case. Inne znaki pozostaw bez zmian.
+            - shift może być dowolną liczbą całkowitą (obsłuż modulo 26). Dopuszczalne wartości ujemne.
+            - Znaki narodowe (np. ą, ę, ł) pozostaw bez zmian.
+         */
+
+        $result = moveCaseSensitiveCharacters($inputText, $offset);
+    }
+
+    public function dataProvider(): \Generator
+    {
+        yield ['ABC', '3', 'DEF'];
+        yield ['xyz', '3', 'abc'];
+    }
+}
