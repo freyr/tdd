12715c1d64399740cd5986e24dadb0a46a34862b
Grzegorz Kuczera <Grzegorz.Kuczera@wakacje.pl>
2025-09-30 11:37:28 +0200

    create red WordWrapTest third step

--- tests/WordWrapTest.php
+++ tests/WordWrapTest.php
@@ -15,11 +15,15 @@ class WordWrapTest extends TestCase
     {
         $w = new WordWrap();
         $this->assertEquals(strlen($w->wrap($text, $width)), $width);
+        $this->assertEquals($w->wrap($text, $width), 'ABC\nD');
+        $this->assertEquals($w->wrap($text, $width), 'A\n B C');
     }
 
     public static function data(): \Generator
     {
         return
             yield 'number_of_sign_in_line' => ['A', 3];
+            yield 'add_enter_if_more_sign' => ['ABCD', 3];
+            yield 'add_enter_before_space' => ['A B C', 3];
     }
 }
