e79c66a65bc556a61a8d356e53ce22233d38f2af
wkulpan <wojciech.kulpan@wakacje.pl>
2025-09-30 11:02:09 +0200

    wkulpan-wakacje

--- src/CesarCipher.php
+++ src/CesarCipher.php
@@ -9,9 +9,36 @@ class CesarCipher
 
     public function encode(string $text, int $shift): string
     {
+
+        $polishChar = [
+            'ą','ć','ę','ł','ń','ó','ś','ź','ż',
+
+            'Ą','Ć','Ę','Ł','Ń','Ó','Ś','Ź','Ż',
+
+            '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+            '-', '_', '=', '+', '[', ']', '{', '}', ';', ':',
+            "'", '"', '\\', '|', ',', '.', '<', '>', '/', '?',
+            '`', '~'
+        ];
+
+        // duze 65 - 90
+        // małe 97 - 122
+
         $result = '';
         for ($i = 0; $i < $shift; $i++) {
-            $result .= chr(ord(substr($text, $i)) + $shift);
+            $char = substr($text, $i);
+            if (in_array($char, $polishChar)) {
+                $result .= $char;
+            } else {
+                $count = ord($char);
+                if (65 < $count && $count < 90) {
+                    $shift = (($shift % 26) + 26) % 26;
+                }
+                $newChar = ord($char) + $shift;
+
+                $shift = (($shift % 26) + 26) % 26;
+                $result .= chr(ord($char) + $shift);
+            }
         }
 
         return $result;
