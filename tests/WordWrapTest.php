3d7c7867d922ac85e8c8a817897360564e0a2701
Katarzyna <kbrodacka@wakacje.pl>
2025-09-30 11:05:59 +0200

    kinga driver

---
+++ tests/WordWrapTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\TDD\Tests;
+
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class WordWrapTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('data')]
+    public function string_add(string $text, int $width, string $result): void
+    {
+        $wordWrap = new WordWrap();
+        self::assertEquals($result, $wordWrap->wrap($text, $width));
+    }
+
+    public static function data(): Generator
+    {
+        yield 'abc_shift' => ['abcdefg', 4, 'abcd\nefg'];
+    }
+}
