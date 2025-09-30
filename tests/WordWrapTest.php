705709c535b0aac468b98c2748561c63e22d6830
wkulpan <wojciech.kulpan@wakacje.pl>
2025-09-30 11:08:28 +0200

    wkulpan-wakacje

---
+++ tests/WordWrapTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\TDD\Tests;
+
+use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class WordWrapTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('data')]
+    public function wrap_test(string $text, int $width, string $result): void
+    {
+        $ww = new WordWrap();
+
+        self::assertEquals($result, $ww->wrap($text, $width));
+    }
+
+    public static function data(): Generator
+    {
+        yield 'wrap_1' => ["word word", 6, "word\nword"];
+        yield 'wrap_2' => ["abcdefghij", 4, "abcd\nefgh\nij"];
+    }
+}
